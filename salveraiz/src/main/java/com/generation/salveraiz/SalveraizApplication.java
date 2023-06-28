c5add880d7c192b72160f1cd446e55084c169ddc
Pedro Henrique Costa <133285730+Pedrohcm1@users.noreply.github.com>
2023-06-28 11:14:28 -0300

    tarefa 3 projeto spring

---
+++ salveraiz/src/main/java/com/generation/salveraiz/SalveraizApplication.java
@@ -0,0 +1,13 @@
+package com.generation.salveraiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalveraizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalveraizApplication.class, args);
+	}
+
+}
---
+++ salveraiz/src/test/java/com/generation/salveraiz/SalveraizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.salveraiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalveraizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
